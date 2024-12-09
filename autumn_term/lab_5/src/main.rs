ad03bda21d6caef25fccadcd92780e68726f0f51
AntonAnisimovMIPT <127052051+AntonAnisimovMIPT@users.noreply.github.com>
2024-12-09 23:08:26 +0300

    fixed errors lab_5

--- autumn_term/lab_5/src/main.rs
+++ autumn_term/lab_5/src/main.rs
@@ -1,12 +1,21 @@
-use std::f64::consts::E;
-
-fn trapezoidal_method(x: &[f64], y: &[f64]) -> f64 {
+fn trapezoidal_method_h(x: &[f64], y: &[f64]) -> f64 {
     let n = x.len() - 1;
     let mut integral = 0.0;
 
     for i in 0..n {
         let h = x[i + 1] - x[i];
         integral += h * (y[i] + y[i + 1]) / 2.0;
+    }
+
+    integral
+}
+
+fn trapezoidal_method_2h(x: &[f64], y: &[f64]) -> f64 {
+    let mut integral = 0.0;
+
+    for i in (0..x.len() - 1).step_by(2) {
+        let h = x[i + 2] - x[i];
+        integral += h * (y[i] + y[i + 2]) / 2.0;
     }
 
     integral
@@ -32,45 +41,28 @@ fn simpson_method(x: &[f64], y: &[f64]) -> f64 {
     integral * h / 3.0
 }
 
-fn runge_refinement(trapezoidal_coarse: f64, trapezoidal_fine: f64) -> f64 {
-    let p = 2.0; 
-    trapezoidal_fine + (trapezoidal_fine - trapezoidal_coarse) / (2_f64.powf(p) - 1.0)
+fn runge_refinement(i_h: f64, i_2h: f64, p: u32) -> f64 {
+    let r: f64 = 2.0; 
+    (r.powi(p as i32) * i_h - i_2h) / (r.powi(p as i32) - 1.0)
 }
-
-fn interpolate(x: &[f64], y: &[f64], xi: f64) -> f64 {
-    for i in 0..x.len() - 1 {
-        if xi >= x[i] && xi <= x[i + 1] {
-            let t = (xi - x[i]) / (x[i + 1] - x[i]);
-            return y[i] * (1.0 - t) + y[i + 1] * t;
-        }
-    }
-    0.0
-}
-
-
 
 fn main() {
-    
-    let x = vec![
-        0.0, 0.125, 0.25, 0.375, 0.5, 0.625, 0.75, 0.875, 1.0];
+
+    let x = vec![0.0, 0.125, 0.25, 0.375, 0.5, 0.625, 0.75, 0.875, 1.0];
     let y = vec![
         0.0, 0.021470, 0.293050, 0.494105, 0.541341, 0.516855, 0.468617, 0.416531, 0.367879,
     ];
 
-    let integral_trapezoidal = trapezoidal_method(&x, &y);
+    let integral_trapezoidal = trapezoidal_method_h(&x, &y);
+    let integral_trapezoidal_fine = trapezoidal_method_2h(&x, &y);
 
-    // Создание более плотной сетки для уточнения
-    let x_fine: Vec<f64> = (0..=16).map(|i| i as f64 / 16.0).collect();
-    let y_fine: Vec<f64> = x_fine.iter().map(|xi| interpolate(&x, &y, *xi)).collect();
-    let integral_trapezoidal_fine = trapezoidal_method(&x_fine, &y_fine);
-    let refined_result = runge_refinement(integral_trapezoidal, integral_trapezoidal_fine);
+    let refined_result = runge_refinement(integral_trapezoidal, integral_trapezoidal_fine, 2);
 
     let integral_simpson = simpson_method(&x, &y);
 
-   
     println!("Интеграл методом трапеций: {:.6}", integral_trapezoidal);
     println!(
-        "Уточненный интеграл методом Рунге: {:.6}",
+        "Уточнённый интеграл методом Рунге: {:.6}",
         refined_result
     );
     println!("Интеграл методом Симпсона: {:.6}", integral_simpson);
